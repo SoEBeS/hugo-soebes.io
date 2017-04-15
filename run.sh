c238cea89f77c13d3340e1d3ff3c372f07c1948a
Karl Heinz Marbaise <khmarbaise@soebes.de>
2017-04-15 20:44:43 +0200

    Fixed run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #hugo server --theme=hugo-redlounge -d public 
-rm -fr public
+##rm -fr public
 #hugo server --theme=blackburn -d public --uglyURLs
-hugo server --theme=hugo-octopress -d public --uglyURLs
+hugo server --theme=hugo-octopress -d public --uglyURLs -b https://soebes.github.io/hugo-soebes.io/
 ##hugo server --theme=hugo-redlounge -d public --uglyURLs

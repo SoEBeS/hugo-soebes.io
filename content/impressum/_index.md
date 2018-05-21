ef017a025a1e55f6564e6731d738415f7a9d8010
Karl Heinz Marbaise <khmarbaise@soebes.de>
2018-05-21 13:53:50 +0200

    Safe state.

---
+++ content/impressum/_index.md
@@ -0,0 +1,7 @@
+---
+title: "Impressum"
+date: 2018-05-12T21:58:09+01:00
+anchor: "impressum"
+weight: 20 
+---
+Hier ist das impressum.
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #hugo server --theme=hugo-redlounge -d public 
 ##rm -fr public
 # hugo server --theme=hugo-octopress -d public --uglyURLs -b https://soebes.github.io/hugo-soebes.io/
-hugo server --buildDrafts --theme=hugo-octopress -d public --uglyURLs --port 3300 
+#hugo server --buildDrafts -d public --port 3300 
+hugo server --buildDrafts --disableFastRender -d public --port 3300 
 ##hugo server --theme=hugo-redlounge -d public --uglyURLs
